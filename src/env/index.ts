79d47f46bbe2a52013acc447f4f46c3198f531a0
Michelon Souza <michelon.souza@hotmail.com>
2025-03-03 18:23:08 -0300

    chore: inital commit

---
+++ src/env/index.ts
@@ -0,0 +1,31 @@
+import { config } from 'dotenv';
+import { z } from 'zod';
+
+function getEnv() {
+  const envs = {
+    test: '.env.test',
+    development: undefined,
+    production: '.env',
+  };
+
+  config({ path: envs[process.env.NODE_ENV as keyof typeof envs] });
+}
+
+getEnv();
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
+  PORT: z.coerce.number().default(3333),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.error('⚠️ Invalid environment variables:', _env.error.format());
+
+  process.exit(1);
+}
+
+export const env = _env.data;
